b4aa5f554a3a843dda990b2d77190eb99277b474
Karan Kaushik <karan@karankaushik.com>
2014-05-20 01:39:23 -0700

    add sync to dropbox functionality

--- app/src/main/java/com/example/simpletodosecond/app/ToDoActivity.java
+++ app/src/main/java/com/example/simpletodosecond/app/ToDoActivity.java
@@ -3,6 +3,7 @@ package com.example.simpletodosecond.app;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -12,6 +13,14 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.dropbox.sync.android.DbxAccountManager;
+import com.dropbox.sync.android.DbxException;
+import com.dropbox.sync.android.DbxFile;
+import com.dropbox.sync.android.DbxFileSystem;
+import com.dropbox.sync.android.DbxPath;
+import com.google.gson.Gson;
+
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class ToDoActivity extends Activity {
@@ -19,7 +28,10 @@ public class ToDoActivity extends Activity {
     ArrayList<String> items;
     ArrayAdapter<String> itemsAdapter;
     ListView lvItems;
+    static final int REQUEST_LINK_TO_DBX = 0;
     private final int REQUEST_CODE = 20;
+    private DbxAccountManager mDbxAcctMgr;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,16 +39,18 @@ public class ToDoActivity extends Activity {
         setContentView(R.layout.activity_to_do);
         items = new ArrayList <String>();
         //find list view
+        mDbxAcctMgr = DbxAccountManager.getInstance(getApplicationContext(), "65vwxcxq5cnsl72", "ue111fdgv4056al");
         lvItems = (ListView) findViewById(R.id.lvitems);
-
         items.add("First item");
         items.add("second item");
+        loadItems();
 
         //Instantiate and create adapter
         itemsAdapter = new ArrayAdapter <String> (this, android.R.layout.simple_list_item_1, items);
 
         //bind adapters to list view
         lvItems.setAdapter(itemsAdapter);
+
 
         //Setup listener for removing an item
         setupListViewListenter();
@@ -72,6 +86,7 @@ public class ToDoActivity extends Activity {
           itemsAdapter.add(eNewItem.getText().toString());
         }
         eNewItem.setText("");
+        saveItems();
     }
 
     private void setupListViewListenter(){
@@ -104,16 +119,102 @@ public class ToDoActivity extends Activity {
         });
     }
 
+    public void linkToDbx(View view) {
+        if (!mDbxAcctMgr.hasLinkedAccount()){
+            mDbxAcctMgr.startLink((Activity)this, REQUEST_LINK_TO_DBX);
+        }
+        Toast.makeText((Activity)this, "Successfully Linked", Toast.LENGTH_SHORT).show();
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         // REQUEST_CODE is defined above
+        if (requestCode == REQUEST_LINK_TO_DBX) {
+            if (resultCode == Activity.RESULT_OK) {
+                try{
+                    DbxPath appPath =  new DbxPath("app.txt");
+                    DbxFileSystem dbxFs = DbxFileSystem.forAccount(mDbxAcctMgr.getLinkedAccount());
+                    DbxFile appFile1 = dbxFs.create(appPath);
+                    DbxFile appFile = dbxFs.open(appPath);
+                    Gson gson = new Gson();
+                    String json = gson.toJson(items);
+                    appFile.writeString(json);
+                } catch(DbxException.Unauthorized e){
+                    Log.e(getLocalClassName(), "not authorized", e);
+                } catch (IOException e){
+                    Log.e(getLocalClassName(), "unable to write to file", e);
+                }
+
+                Toast.makeText((Activity)this, "Successfully Linked and created file", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText((Activity)this, "Link Failed", Toast.LENGTH_SHORT).show();
+            }
+        }
         if (resultCode == RESULT_OK && requestCode == REQUEST_CODE) {
             String name = data.getExtras().getString("name");
             int position = data.getExtras().getInt("position");
             items.set(position, name);
+            saveItems();
             itemsAdapter.notifyDataSetChanged();
 
+
         }
+    }
+
+    private void loadItems() {
+
+        DbxFile dbxFile = getFile();
+        if (dbxFile == null){
+            return;
+        }
+        try {
+            Gson gson = new Gson();
+            items = gson.fromJson(dbxFile.readString(), ArrayList.class);
+        } catch (IOException e) {
+            items = new ArrayList<String>();
+            Log.w(getLocalClassName(), "unable to load items", e);
+        } finally{
+            dbxFile.close();
+        }
+    }
+
+    private void saveItems() {
+        DbxFile dbxFile = getFile();
+        if (dbxFile == null){
+            return;
+        }
+        try {
+            Gson gson = new Gson();
+            String json = gson.toJson(items);
+            dbxFile.writeString(json);
+        } catch (IOException e) {
+            Log.e(getLocalClassName(), "unable to save items", e);
+        }finally{
+            dbxFile.close();
+        }
+    }
+
+    private DbxFile getFile() {
+        DbxFileSystem dbxFs = null;
+        try{
+           if (mDbxAcctMgr.hasLinkedAccount()){
+              dbxFs = DbxFileSystem.forAccount(mDbxAcctMgr.getLinkedAccount());
+            }else{
+                return null;
+            }
+        } catch(DbxException.Unauthorized e){
+            Log.e(getLocalClassName(), "not authorized", e);
+            return null;
+        }
+        DbxPath appPath =  new DbxPath("app.txt");
+        DbxFile appFile = null;
+        try {
+         appFile = dbxFs.open(appPath);
+        } catch (DbxException e2) {
+              Log.e(getLocalClassName(), "unable to open file", e2);
+        }
+
+        return appFile;
     }
 
 
